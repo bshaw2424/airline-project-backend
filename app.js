f640d81dd0e498b5c9e85db267130074b9779b0d
Bishop Shaw III <prototype124@yahoo.com>
2022-09-27 18:40:25 -0700

    app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const airlineRoutes = require("./routes/airlines");
 //const destinations = require("./controllers/destinations");
+require("./databaseConnect");
 const PORT = process.env.port || 8000;
 
 app.set("view engine", "ejs");
