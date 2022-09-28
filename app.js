26976a43411502415397179b91a01b41e23d3926
Bishop Shaw III <prototype124@yahoo.com>
2022-09-27 17:47:17 -0700

    app.js file

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const airlineRoutes = require("./routes/airlines");
+//const destinations = require("./controllers/destinations");
+const PORT = process.env.port || 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use("/", airlineRoutes);
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
