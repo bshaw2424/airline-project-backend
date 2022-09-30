738f02158a7ba410bdc348dfabba64ae8bc9201b
Bishop Shaw III <prototype124@yahoo.com>
2022-09-29 18:08:22 -0700

    app.js update

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const path = require("path");
 const airlineRoutes = require("./routes/airlines");
 //const destinations = require("./controllers/destinations");
 require("./databaseConnect");
+const methodOverride = require("method-override");
 const PORT = process.env.port || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
