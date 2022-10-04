14985bb30a4cfea97692ee00357e7e55c350abfc
Bishop Shaw III <prototype124@yahoo.com>
2022-10-03 20:13:25 -0700

    app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.use("/", airlineRoutes);
-app.use("/:slug/destination", destinationRoutes);
+app.use("/airline", airlineRoutes);
+app.use("/airline/:slug/destinations", destinationRoutes);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
