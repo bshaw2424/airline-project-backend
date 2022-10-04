f4596c594b5ffc72fce53c2ee5c6ce5aa33d1904
Bishop Shaw III <prototype124@yahoo.com>
2022-10-03 20:16:56 -0700

    airline controller

--- controllers/airlines.js
+++ controllers/airlines.js
@@ -1,11 +1,51 @@
-const airlinesController = require("../models/airlines");
+const Airlines = require("../models/airlines");
 
 module.exports.index = async (req, res) => {
-  res.render("destinations");
+  const airlines = await Airlines.find({});
+  res.render("airlines/index", { airlines });
 };
 
 module.exports.newAirline = async (req, res) => {
   res.render("airlines/new");
 };
 
-module.exports.post = async (req, res) => {};
+module.exports.post = async (req, res) => {
+  const { Airline } = req.body;
+  const newAirline = new Airlines(Airline);
+  await newAirline.save();
+  res.redirect("/airline");
+};
+
+module.exports.showPage = async (req, res) => {
+  const { slug } = req.params;
+  const showPage = await Airlines.findOne({ slug });
+  res.render("airlines/showPage", { showPage });
+};
+
+module.exports.edit = async (req, res) => {
+  const { slug } = req.params;
+  const airlines = await Airlines.findOne({ slug: slug });
+  res.render("airlines/edit", { airlines });
+};
+
+module.exports.update = async (req, res) => {
+  const { slug } = req.params;
+  const { Airline } = req.body;
+  const airline = await Airlines.findOneAndUpdate(
+    {
+      slug: slug,
+    },
+    { ...Airline },
+    {
+      new: true,
+    },
+  );
+  await airline.save();
+  res.redirect("/airline");
+};
+
+module.exports.delete = async (req, res) => {
+  const { slug } = req.params;
+  await Airlines.findOneAndDelete({ slug: slug });
+  res.redirect("/airline");
+};
